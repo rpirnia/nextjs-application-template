57abdca52c68bb7edc636fe8030948cfbebda95e
Ruhi Pirnia <147004419+rpirnia@users.noreply.github.com>
2025-03-14 21:40:45 -1000

    remove button implemented in 20 minutes

--- src/components/StuffItem.tsx
+++ src/components/StuffItem.tsx
@@ -1,6 +1,14 @@
+'use client';
+
 import { Stuff } from '@prisma/client';
 import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { Trash } from 'react-bootstrap-icons';
+import { deleteStuff } from '@/lib/dbActions';
 
+const removeItem = (id: number) => {
+  deleteStuff(id);
+};
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
 const StuffItem = ({ name, quantity, condition, id }: Stuff) => (
   <tr>
@@ -9,6 +17,11 @@ const StuffItem = ({ name, quantity, condition, id }: Stuff) => (
     <td>{condition}</td>
     <td>
       <Link href={`/edit/${id}`}>Edit</Link>
+    </td>
+    <td>
+      <Button variant="danger" onClick={() => removeItem(id)}>
+        <Trash />
+      </Button>
     </td>
   </tr>
 );
